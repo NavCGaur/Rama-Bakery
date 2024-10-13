19fdc1fc46bb5eca2290790758baad121e8fa480
NavCGaur <vertigo1112@gmail.com>
2024-10-13 19:11:06 +0530

    commit all

--- frontend/src/container/main/Main.jsx
+++ frontend/src/container/main/Main.jsx
@@ -9,7 +9,7 @@ import './Main.css';
 import DropDown from '../../components/dropdown/DropDown';
 
 function Main() {
-  const [menuStatus, setMenuStatus] = useState(false);
+  const [ setMenuStatus] = useState(false);
   const [searchStatus, setSearchStatus] = useState(false);
   const [searchQuery, setSearchQuery] = useState('');
   const [filteredProducts, setFilteredProducts] = useState([]);
