e6cf33157440fcef0e810ef08891bf75da730457
NavCGaur <vertigo1112@gmail.com>
2024-10-14 17:33:06 +0530

    commit validate.js

--- frontend/src/auth/utils/validatePassword.js
+++ frontend/src/auth/utils/validatePassword.js
@@ -1,25 +1,28 @@
- const validatePassword = (password) => {
-    const errors = [];
-    const passMinLength = 8;
-  
-    if (password.length < passMinLength) {
-      errors.push(`Password should have a minimum length of ${passMinLength}`);
-    }
-    if (!/[A-Za-z]/.test(password)) {
-      errors.push('Password must contain at least one alphabet');
-    }
-    if (!/\d/.test(password)) {
-      errors.push('Password must contain at least one number');
-    }
-    if (!/[!@#$%^&*]/.test(password)) {
-      errors.push('Password must contain at least one special character');
-    }
-    if (/\s/.test(password)) {
-      errors.push('Password cannot contain spaces');
-    }
-  
-    return errors;
-  };
-  
+const validatePassword = (password, confirmPassword) => {
+  const errors = {};
 
-export default validatePassword
+  const passMinLength = 8;
+
+  if (password.length < passMinLength) {
+    errors.password = `Password should have a minimum length of ${passMinLength}`;
+  }
+  if (!/[A-Za-z]/.test(password)) {
+    errors.password = 'Password must contain at least one alphabet';
+  }
+  if (!/\d/.test(password)) {
+    errors.password = 'Password must contain at least one number';
+  }
+  if (!/[!@#$%^&*]/.test(password)) {
+    errors.password = 'Password must contain at least one special character';
+  }
+  if (/\s/.test(password)) {
+    errors.password = 'Password cannot contain spaces';
+  }
+  if (password !== confirmPassword) {
+    errors.confirmPassword = 'Passwords do not match';
+  }
+
+  return errors;
+};
+
+export default validatePassword;
