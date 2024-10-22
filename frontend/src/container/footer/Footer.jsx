04872de0ed4db81b5bd35dac7c634fa95cb200bb
NavCGaur <vertigo1112@gmail.com>
2024-10-22 10:03:43 +0530

    commit all

--- frontend/src/container/footer/Footer.jsx
+++ frontend/src/container/footer/Footer.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 import './Footer.css'
 
+import { Link } from 'react-router-dom';
+
+
+import GoogleMap from './GoogleMap'
+
 import {ReactComponent as Phone} from '../../assets/phone-solid.svg'
 import {ReactComponent as Email} from '../../assets/email.svg'
+import {ReactComponent as Location} from '../../assets/location.svg'
+import {ReactComponent as Instagram} from '../../assets/instagram.svg'
+import {ReactComponent as Facebook} from '../../assets/facebook.svg'
+import {ReactComponent as Whatsapp} from '../../assets/whatsapp.svg'
+
+
 
 function Footer() {
   return (
@@ -11,31 +22,65 @@ function Footer() {
         <div className='footer__top'>
 
             <div className='footer__contact-group'>
-                <p>Contact</p>
+                <h3>Contact</h3>
                 <div className='footer__contact'>
                     <Email className='footer__contact-icon'/>
-                    <p contact__email-text>ramabakery@gmail.com</p>
+                    <Link to='mailto:naveencg070@gmail.com'><p contact__email-text>ramabakery@gmail.com</p></Link>
                 </div>
 
                 <div className='footer__contact'>
                     <Phone className='footer__contact-icon'/>
-                    <p> +91 9920899845</p>
+                    <Link to='tel:+919920899845'><p> +91 9920899845</p></Link>
+                </div>
+
+                <div className='footer__contact'>
+                    <Location className='footer__contact-icon'/>
+                    <div className='footer__contact-location'>
+                        <p>54, High Street, Borivali East  </p>
+                        <p>Mumbai - 401107  </p>
+                    </div>
+
                 </div>
 
             </div>
             <div className='footer__workTime'>
-                <p>Work Time</p>
+                <h3>Work Time</h3>
+                <p>Our Work Timing are:</p>
+                <p>Monday - Friday : 8 AM to 8 PM </p>
+                <p>Saturday : 10 AM to 10 PM </p>
+                <p>Sunday : Closed </p>
             </div>
+
+
+            <div className='footer__social'>
+                <div className='footer__social-icons'>
+                    <Link to='https://www.facebook.com/' target='_blank'><Facebook className="footer__social-icons"/></Link>        
+                </div>
+                <div className='footer__social-icons'>
+                    <Link to='https://www.instagram.com/' target='_blank'><Instagram className="footer__social-icons"/></Link>     
+                </div>
+                <div className='footer__social-icons'>
+                    <Link to='https://wa.me/919920899845' target='_blank'><Whatsapp className="footer__social-icons"/></Link>        
+                </div>
+
+             </div>
+
+
         
             <div className='footer__map'>
-
+                <GoogleMap />
             </div>
+        
         </div>
+
+        
 
         <div className='footer__bottom'>
-            <p>copyright</p>
+            <p>2024 © All Rights Reserved. Designed and Developed by Naveen Gaur.</p>
         </div>
-    
+
+
+
     </div>
   )
 }
---
+++ frontend/src/container/footer/GoogleMap.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './GoogleMap.css'
+
+const GoogleMap = () => {
+  return (
+    <div className='footer__map'>
+            <div className="footer__google-map">
+              <iframe 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30137.288008990927!2d72.82510433518108!3d19.231813819282113!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b128b333e163%3A0x985640540577af7e!2sBorivali%2C%20Mumbai%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1721273737535!5m2!1sen!2sin" 
+                style={{ border: 0 }} 
+                allowFullScreen 
+                loading="lazy" 
+                referrerPolicy="no-referrer-when-downgrade" 
+                title='location map'
+                >
+              </iframe>
+            </div>
+        </div>
+  );
+};
+
+export default GoogleMap;
