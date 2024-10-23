ff7b90fcb69b1c4637b30cd5e9b746653c3497b8
NavCGaur <vertigo1112@gmail.com>
2024-10-23 06:32:23 +0530

    commit all

--- frontend/src/container/testimonials/data.js
+++ frontend/src/container/testimonials/data.js
@@ -18,18 +18,18 @@ const testimonialsData = [  {
                             review: "Can't thank you enough for doing such wonderful work on our wedding cake. Everything was beautiful and tasted wonderful. Thank you for all the time, care and effort.",                       
                             } ,
                             {
-                                image: testimonialImage3,
-                                review: "You guys made my sons birthday a great one. He loved his lion King cake. Also, your extra service of holding the cake an extra day after not having a fridge large enough. We will be back on many more occasions. Thanks for everything.",                       
-                                },
-                                {
-                                image: testimonialImage2,
-                                review: "Your Cake was fabulous! Every piece was savored. Wish we ordered more... Thanks for delivering all the way up to Ranch . We appreciate your service.",                       
-                                },
+                            image: testimonialImage3,
+                            review: "You guys made my sons birthday a great one. He loved his lion King cake. Also, your extra service of holding the cake an extra day after not having a fridge large enough. We will be back on many more occasions. Thanks for everything.",                       
+                            },
+                            {
+                            image: testimonialImage2,
+                            review: "Your Cake was fabulous! Every piece was savored. Wish we ordered more... Thanks for delivering all the way up to Ranch . We appreciate your service.",                       
+                            },
             
-                                {
-                                image: testimonialImage1,
-                                review: "Can't thank you enough for doing such wonderful work on our wedding cake. Everything was beautiful and tasted wonderful. Thank you for all the time, care and effort.",                       
-                                }  
+                            {
+                            image: testimonialImage1,
+                            review: "Can't thank you enough for doing such wonderful work on our wedding cake. Everything was beautiful and tasted wonderful. Thank you for all the time, care and effort.",                       
+                            }  
                         
 
                        
