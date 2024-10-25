8230c5b34ae0978eede7d54581189dc44ecda772
NavCGaur <vertigo1112@gmail.com>
2024-10-25 13:36:07 +0530

    commit all

--- frontend/src/container/footer/Map.jsx
+++ frontend/src/container/footer/Map.jsx
@@ -2,23 +2,21 @@ import React from 'react';
 import './Map.css';
 
 const Map = () => {
+  const handleMapClick = () => {
+    window.open("https://www.openstreetmap.org/?mlat=19.231813819282113&mlon=72.82510433518108#map=12/19.2318/72.8251", "_blank");
+  };
 
   return (
     <div className='footer__map'>
-      <a 
-        href="https://www.openstreetmap.org/?mlat=19.231813819282113&mlon=72.82510433518108#map=12/19.2318/72.8251" 
-        target="_blank" 
-        rel="noopener noreferrer" 
-        className="footer__google-map"
-      >
+      <div className=".footer__map-link" onClick={handleMapClick} style={{ cursor: 'pointer' }}>
         <iframe 
           src="https://www.openstreetmap.org/export/embed.html?bbox=72.82510433518108%2C19.231813819282113%2C72.82510433518108%2C19.231813819282113&layer=mapnik"
-          style={{ border: 0}} 
+          style={{ border: 0 }} // Adjust the height as needed
           allowFullScreen
           loading="lazy"
           title="Our Location on OpenStreetMap"
         ></iframe>
-      </a>
+      </div>
     </div>
   );
 };
