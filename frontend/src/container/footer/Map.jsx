64c8da84a6f27c3e6bccee862e4c9e7bf0e5ea26
NavCGaur <vertigo1112@gmail.com>
2024-10-25 13:23:50 +0530

    commit all

--- frontend/src/container/footer/Map.jsx
+++ frontend/src/container/footer/Map.jsx
@@ -6,16 +6,19 @@ const Map = () => {
 
   return (
     <div className='footer__map'>
-      <a href={mapUrl} target="_blank" rel="noopener noreferrer">
-        <div className="footer__map-link">
-          <iframe 
-            src="https://www.openstreetmap.org/export/embed.html?bbox=72.82510433518108%2C19.231813819282113%2C72.82510433518108%2C19.231813819282113&layer=mapnik"
-            style={{ border: 0}} 
-            allowFullScreen
-            loading="lazy"
-            title="Our Location on OpenStreetMap"
-          ></iframe>
-        </div>
+      <a 
+        href="https://www.openstreetmap.org/?mlat=19.231813819282113&mlon=72.82510433518108#map=12/19.2318/72.8251" 
+        target="_blank" 
+        rel="noopener noreferrer" 
+        className="footer__google-map"
+      >
+        <iframe 
+          src="https://www.openstreetmap.org/export/embed.html?bbox=72.82510433518108%2C19.231813819282113%2C72.82510433518108%2C19.231813819282113&layer=mapnik"
+          style={{ border: 0}} 
+          allowFullScreen
+          loading="lazy"
+          title="Our Location on OpenStreetMap"
+        ></iframe>
       </a>
     </div>
   );
