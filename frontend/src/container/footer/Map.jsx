3203afbb3eaac26d7169b26ffc82d8b560e9525e
NavCGaur <vertigo1112@gmail.com>
2024-10-25 13:29:13 +0530

    commit all

--- frontend/src/container/footer/Map.jsx
+++ frontend/src/container/footer/Map.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './Map.css';
 
 const Map = () => {
-  const mapUrl = "https://www.openstreetmap.org/?mlat=19.231813819282113&mlon=72.82510433518108#map=12/19.2318/72.8251";
 
   return (
     <div className='footer__map'>
